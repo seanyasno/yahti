a25be519d02ccb5c6220e5361f59d66936c2cc5f
Sean Yasnogrodski <sean.yasno@gmail.com>
2022-10-20 13:38:18 +0300

    Worked a little on activity item in home page.

--- src/components/activity-item/activity-item.tsx
+++ src/components/activity-item/activity-item.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import {Activity} from '@abstraction/types';
 import styled from '@emotion/styled';
-import {Card, Typography} from '@mui/material';
+import {Avatar, Card, IconButton, Stack, Typography} from '@mui/material';
+import {emojiByActivityType} from '@constants/index';
+import {GoLinkExternal} from 'react-icons/go';
+import {openUrlInNewTab} from '@utils/index';
 
 export const Container = styled(Card)`
   border-radius: 1em;
   padding: 16px 20px;
+`;
+
+export const ActivityType = styled(Avatar)`
+  background-color: #F1F1F1;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  width: 24px;
+  height: 24px;
+  font-size: 12px;
 `;
 
 type Props = {
@@ -17,8 +28,20 @@ export const ActivityItem: React.FC<Props> = (props) => {
 
     return (
         <Container elevation={0}>
-            <Typography variant={'h6'} fontWeight={600}>{activity.title}</Typography>
-            <Typography variant={'body2'}>{activity.description}</Typography>
+            <Typography mb={'4px'} variant={'h6'} fontWeight={600}>{activity.title}</Typography>
+
+            <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'} textAlign={'center'}>
+                    <ActivityType>
+                        {emojiByActivityType[activity.type]}
+                    </ActivityType>
+
+                {
+                    activity.link &&
+                    <IconButton sx={{padding: 0}} onClick={() => openUrlInNewTab(activity.link)}>
+                        <GoLinkExternal size={'20px'}/>
+                    </IconButton>
+                }
+            </Stack>
         </Container>
     );
 };
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export const openUrlInNewTab = (url: string) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+};
