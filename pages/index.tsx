6fa4a5ae899731cef2d0fc42c838ced0b819d1b3
Sean Yasnogrodski <sean.yasno@gmail.com>
2022-10-28 18:11:21 +0300

    Added done and not done activities tab switcher.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type {NextPage} from 'next';
 import {Container, Stack, Typography, Fab, Tabs, Tab, Box} from '@mui/material';
-import {activitiesMock} from '@mocks/index';
 import {ActivityItem} from '@components/index';
 import {MdAdd} from 'react-icons/md';
 import {useRouter} from 'next/router';
@@ -8,20 +7,47 @@ import {useQuery} from '@tanstack/react-query';
 import {collection, getDocs} from '@firebase/firestore';
 import {db} from '@config/index';
 import {Activity} from '@abstraction/types';
-import Link from 'next/link';
+import styled from '@emotion/styled';
+import {theme} from '@styles/index';
+import {useMemo, useState} from 'react';
+
+export const StyledTabs = styled(Tabs)`
+  margin: 0 0 20px 0;
+
+  & .MuiTabs-indicator {
+    display: none;
+    background-color: red;
+  }
+`;
+
+export const StyledTab = styled(Tab)`
+  color: ${theme.palette.text.primary};
+  font-size: 1rem;
+
+  &.Mui-selected {
+    color: ${theme.palette.text.secondary};
+    background-color: ${theme.palette.secondary.main};
+    border-radius: 2em;
+    font-weight: 700;
+    font-size: 1rem;
+  }
+`;
 
 const Home: NextPage = () => {
     const router = useRouter();
+    const [currentTab, setCurrentTab] = useState(0);
     const {data: activities = []} = useQuery({
         queryKey: ['activities'],
         queryFn: async () => {
-            const querySnapshot = await getDocs(collection(db, "activities"));
+            const querySnapshot = await getDocs(collection(db, 'activities'));
             return querySnapshot.docs.map((doc) => ({
                 activity: doc.data(),
                 id: doc.id,
-            })) as {activity: Activity, id: string}[];
+            })) as { activity: Activity, id: string }[];
         },
     });
+    const doneActivities = useMemo(() => activities.filter(({activity}) => activity.done), [activities]);
+    const notDoneActivities = useMemo(() => activities.filter(({activity}) => !activity.done), [activities]);
 
     const title = 'יואואוו שלום';
 
@@ -31,26 +57,30 @@ const Home: NextPage = () => {
         <Container maxWidth={'sm'} sx={{padding: '30px 20px'}}>
             <Typography variant={'h4'} fontWeight={600} mb={'18px'}>{title}</Typography>
 
-            {/*<Tabs*/}
-            {/*    value={0}*/}
-            {/*    variant={'fullWidth'}*/}
-            {/*    textColor={'secondary'}*/}
-            {/*    indicatorColor={'secondary'}>*/}
-            {/*    <Tab label={'One'}/>*/}
-            {/*    <Tab label={'two'}/>*/}
-            {/*</Tabs>*/}
+            <StyledTabs
+                value={currentTab}
+                variant={'fullWidth'}
+                onChange={(event, newValue) => setCurrentTab(newValue)}
+            >
+                <StyledTab label={'מה נעשה'} value={0}/>
+                <StyledTab label={'מה עשינו'} value={1}/>
+            </StyledTabs>
 
             <Stack
                 direction={'column'}
                 spacing={2}
             >
-                {
-                    activities.map(({activity, id}, index) =>
-                        <Box sx={{zIndex: 1}} onClick={() => router.push(`/activity/${id}`)} key={index}>
-                            <ActivityItem id={id} activity={activity}/>
-                        </Box>
-                    )
-                }
+                {currentTab === 0 && notDoneActivities.map(({activity, id}, index) =>
+                    <Box sx={{zIndex: 1}} onClick={() => router.push(`/activity/${id}`)} key={index}>
+                        <ActivityItem id={id} activity={activity}/>
+                    </Box>
+                )}
+
+                {currentTab === 1 && doneActivities.map(({activity, id}, index) =>
+                    <Box sx={{zIndex: 1}} onClick={() => router.push(`/activity/${id}`)} key={index}>
+                        <ActivityItem id={id} activity={activity}/>
+                    </Box>
+                )}
             </Stack>
 
             <Fab color={'secondary'} aria-label="add" onClick={onAddActivity}>
