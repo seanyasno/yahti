6542f0a83ee87c62d599078e63bc90868bdc483a
Sean Yasnogrodski <sean.yasno@gmail.com>
2022-11-15 18:50:38 +0200

    Added home drawer with sign out option.

---
+++ src/components/home-drawer/home-drawer.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from 'react';
+
+import { FiLogOut } from 'react-icons/fi';
+
+import { auth } from '@config/index';
+import { Button, SwipeableDrawer } from '@mui/material';
+
+type Props = {
+    open: boolean;
+    onClose: () => void;
+    onOpen: () => void;
+};
+
+export const HomeDrawer: React.FC<Props> = (props) => {
+    const { open, onClose, onOpen } = props;
+
+    const signOutButtonTitle = 'להתנתק';
+
+    const onSignOut = useCallback(async () => {
+        try {
+            await auth.signOut();
+        } catch (error) {
+            console.error(error);
+        }
+    }, [auth]);
+
+    return (
+        <SwipeableDrawer
+            anchor={'left'}
+            open={open}
+            onClose={onClose}
+            onOpen={onOpen}
+            PaperProps={{
+                sx: {
+                    minWidth: '140px',
+                },
+            }}
+        >
+            <Button color={'secondary'} onClick={onSignOut}>
+                <FiLogOut style={{ margin: '0 0 0 4px' }} />{' '}
+                {signOutButtonTitle}
+            </Button>
+        </SwipeableDrawer>
+    );
+};
