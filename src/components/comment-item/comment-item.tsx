82b0503aa3653da7f3ec5ebffa81aeaa7b30cb86
Sean Yasnogrodski <sean.yasno@gmail.com>
2022-11-16 15:01:49 +0200

    Added user names in comments.

--- src/components/comment-item/comment-item.tsx
+++ src/components/comment-item/comment-item.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 import { Typography } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 import TimeAgo from 'javascript-time-ago';
 
 import { Comment } from '@abstraction/types';
+import { fetchUserDetailsByEmail } from '@requests/index';
 
 export const Card = styled.div`
     background-color: #fff;
@@ -21,10 +23,16 @@ export const CommentItem: React.FC<Props> = (props) => {
     const { comment } = props;
     const timeAgo = new TimeAgo('he');
 
+    const { data: user } = useQuery({
+        queryKey: ['user', comment.authorId],
+        queryFn: async () => fetchUserDetailsByEmail(comment.authorId),
+    });
+
     return (
         <Card>
+            <Typography variant={'subtitle2'}>{user?.name}</Typography>
             <Typography variant={'body1'}>{comment.content}</Typography>
-            <Typography variant={'body2'} color={'#7e7e7e'}>
+            <Typography variant={'caption'} color={'#7e7e7e'}>
                 {timeAgo.format(comment.createdAt)}
             </Typography>
         </Card>
