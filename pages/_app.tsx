3fbc7cf80977aa4d6e6003546a2d1495a36d9455
seansyasno <sean.yasno@gmail.com>
2023-09-20 22:39:18 +0300

    yes.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -73,7 +73,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
             <CacheProvider value={cacheRtl}>
                 <ThemeProvider theme={theme}>
                     <CssBaseline />
-                    <div>{window?.Notification?.permission}</div>
                     <Component {...pageProps} />
                 </ThemeProvider>
             </CacheProvider>
